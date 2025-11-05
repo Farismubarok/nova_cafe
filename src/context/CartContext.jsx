74f7e5322661fe2afc4e5c9f0fb72af88dc38c72
Faris <mukhammadfarismubarok@gmail.com>
2025-11-05 21:41:55 +0700

    add back end

--- src/context/CartContext.jsx
+++ src/context/CartContext.jsx
@@ -80,23 +80,23 @@ export function CartProvider({ children }) {
   );
 }
 
-import React, { createContext, useState, useContext } from "react";
+// import React, { createContext, useState, useContext } from "react";
 
-const CartContext = createContext();
+// const CartContext = createContext();
 
-export const CartProvider = ({ children }) => {
-  const [cart, setCart] = useState([]);
+// export const CartProvider = ({ children }) => {
+//   const [cart, setCart] = useState([]);
 
-  const addToCart = (item) => setCart((prev) => [...prev, item]);
-  const removeFromCart = (id) => setCart((prev) => prev.filter((i) => i.id !== id));
-  const clearCart = () => setCart([]);
+//   const addToCart = (item) => setCart((prev) => [...prev, item]);
+//   const removeFromCart = (id) => setCart((prev) => prev.filter((i) => i.id !== id));
+//   const clearCart = () => setCart([]);
 
-  return (
-    <CartContext.Provider value={{ cart, addToCart, removeFromCart, clearCart }}>
-      {children}
-    </CartContext.Provider>
-  );
-};
+//   return (
+//     <CartContext.Provider value={{ cart, addToCart, removeFromCart, clearCart }}>
+//       {children}
+//     </CartContext.Provider>
+//   );
+// };
 
 
-export const useCart = () => useContext(CartContext);
+// export const useCart = () => useContext(CartContext);
